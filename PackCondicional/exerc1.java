25d1df9667e569cf6be5929cbc4d94406572d573
Student <letlaurens@email.com>
2020-07-09 11:23:12 -0300

    Exercícios Estrutura Condicional

--- PackCondicional/exerc1.java
+++ PackCondicional/exerc1.java
@@ -6,11 +6,11 @@ public class exerc1 {
 	public static void main (String args[]) {
 		Scanner numero = new Scanner(System.in);
 		int n1,n2,n3;
-		System.out.printf("Digite o primeiro número inteiro: ");
+		System.out.printf("Digite o 1º número inteiro: ");
 		n1=numero.nextInt();
-		System.out.printf("Digite o segundo número inteiro: ");
+		System.out.printf("Digite o 2º número inteiro: ");
 		n2=numero.nextInt();
-		System.out.printf("Digite o terceiro número inteiro: ");
+		System.out.printf("Digite o 3º número inteiro: ");
 		n3=numero.nextInt();
 		if(n1>n2 && n1>n3) {
 			System.out.println("Número "+n1+ " é o maior!");
